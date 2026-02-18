c5bf2816c0c66c55997489aa7ae2bdbbba0d2dfe
AS Ayman <ASAYMAN69>
2026-02-19 02:35:12 +0600

    ehhreuiewiruowiewuieyriuwryiuewrewrwerrwrewr

--- a.sh
+++ a.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-g++ -std=c++17 -o a_cpp main.cpp handlers.cpp http_response.cpp utils.cpp logging.cpp \
+g++ -std=c++17 -o a main.cpp handlers.cpp http_response.cpp utils.cpp logging.cpp \
     -Wall -Wextra -O2 -D_POSIX_C_SOURCE=200809L
 
 if [ $? -eq 0 ]; then
     echo "Compilation successful! Executable created: a_cpp"
-    ls -lh a_cpp
+    ls -lh a
 else
     echo "Compilation failed!"
     exit 1
